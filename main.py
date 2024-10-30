10104b66205f30466284ff21743950d215a880f8
o9u2 <oguz@karaardic.com>
2024-10-30 07:53:18 +0100

    Defined functions for code. Changed to handling integers instead of strings.

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-input_digits = []
+def extract_digits(filename):
+    input_digits = []
 
-with open('input.txt', 'r') as f:
-    # 'For'loop for less memory usage.
-    for line in f:
-        # Allows for list only including strings w/ integer(s).
-        if any(char.isdigit() for char in line):
-            # Extract only digits and concatenate them
-            digits = ''.join([char for char in line if char.isdigit()])
-            input_digits.append(digits)
-print(input_digits)
+    with open(filename, 'r') as f:
+        # 'For' loop for less memory usage.
+        for line in f:
+            # Allows for list only including strings with integer(s).
+            if any(char.isdigit() for char in line):
+                # Concatenate digits as integer for less memory usage.
+                digits = int(''.join([char for char in line if char.isdigit()]))
+                input_digits.append(digits)
+    
+    return input_digits
+
+# def integer_concatination(input_digits):
+    
+
+def main():
+    filename = 'input.txt'
+    digits = extract_digits(filename)
+    print(digits)
+
+if __name__ == "__main__":
+    main()
