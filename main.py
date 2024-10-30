61b4ccff79173065a1fd73df478e83a2676bcb24
o9u2 <oguz@karaardic.com>
2024-10-30 22:26:21 +0100

    Made concat first and last digit function to be a list comprehension. Used slices so single values of calibration integers are repeated.

--- main.py
+++ main.py
@@ -1,32 +1,28 @@
 def extract_input_numbers(filename):
     input_numbers = []
-
-    with open(filename) as f:                                                               # 'For' loop to prevent loading all data into memory.
-        for line in f:                                                                      # Creating list only digit(s).
-            if any(char.isdigit() for char in line):
-                number = ''.join([char for char in line if char.isdigit()])                 
-                number_digits = list(number)
-                digits = [char for char in number_digits]                                   
+    with open(filename) as f:
+        for line in f:
+            digits = [char for char in line if char.isdigit()]
+            if digits:
                 input_numbers.append(digits)
     return input_numbers
 
 
 def concat_first_last_digit(list_of_numbers):
-    concatted_list = []
-    for i, inner_list in enumerate(list_of_numbers):
-        first_digit = inner_list[0]
-        last_digit = inner_list[-1]
-        concatenated = int(first_digit + last_digit)                                        # Convert strings to integers for less memory usage and numerical operations.
-        concatted_list.append([concatenated])
-    return concatted_list
+    return [int(inner_list[0] + inner_list[-1]) for inner_list in list_of_numbers]
 
+
+# def sum_calibrators(calibration_values):
+#     sum_calibration_values = sum(calibration_values)
+#     return sum_calibration_values
 
 def main():
     filename = 'input.txt'
     list_of_numbers = extract_input_numbers(filename)
-    first_and_last = concat_first_last_digit(list_of_numbers)
-    calibration_sum = sum(first_and_last)
-    print(calibration_sum)
+    print(str(list_of_numbers) + '\n')
+    calibration_values = concat_first_last_digit(list_of_numbers)
+    print(calibration_values)
+    # print(sum_calibrators(calibration_values))
 
 if __name__ == "__main__":
     main()
