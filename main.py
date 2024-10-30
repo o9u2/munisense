00094b8fdfeee5f80a06361feaa0906b02fcb2bd
o9u2 <oguz@karaardic.com>
2024-10-30 20:01:43 +0100

    Added function to create list with first and last digit of each number as integer for summation.

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 def extract_input_numbers(filename):
     input_numbers = []
 
-    with open(filename, 'r') as f:
-        # 'For' loop for less memory usage.
-        for line in f:                                                                      # Allows for list only including strings with digit(s).
+    with open(filename) as f:                                                               # 'For' loop to prevent loading all data into memory.
+        for line in f:                                                                      # Creating list only digit(s).
             if any(char.isdigit() for char in line):
-                number = ''.join([char for char in line if char.isdigit()])                 # To create a list of numbers by omitting letters.
+                number = ''.join([char for char in line if char.isdigit()])                 
                 number_digits = list(number)
-                digits = [int(char) for char in number_digits]
+                digits = [char for char in number_digits]                                   
                 input_numbers.append(digits)
     return input_numbers
+
+
+def concat_first_last_digit(list_of_numbers):
+    concatted_list = []
+    for i, inner_list in enumerate(list_of_numbers):
+        first_digit = inner_list[0]
+        last_digit = inner_list[-1]
+        concatenated = int(first_digit + last_digit)                                        # Convert strings to integers for less memory usage and numerical operations.
+        concatted_list.append([concatenated])
+    return concatted_list
+
 
 def main():
     filename = 'input.txt'
     list_of_numbers = extract_input_numbers(filename)
-    print(list_of_numbers)
+    first_and_last = concat_first_last_digit(list_of_numbers)
+    calibration_sum = sum(first_and_last)
+    print(calibration_sum)
 
 if __name__ == "__main__":
     main()
