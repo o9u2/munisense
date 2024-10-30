40d7559d5318a38bfe23f2c9c601b4c1f1c28eeb
o9u2 <oguz@karaardic.com>
2024-10-30 08:49:01 +0100

    Undid to_integer from string of numbers, so split() is possible to create lists within a list for slicing purposes.

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
-def extract_digits(filename):
-    input_digits = []
+def extract_input_numbers(filename):
+    input_numbers = []
 
     with open(filename, 'r') as f:
         # 'For' loop for less memory usage.
-        for line in f:
-            # Allows for list only including strings with integer(s).
+        for line in f:                                                                      # Allows for list only including strings with digit(s).
             if any(char.isdigit() for char in line):
-                # Concatenate digits as integer for less memory usage.
-                digits = int(''.join([char for char in line if char.isdigit()]))
-                input_digits.append(digits)
-    
-    return input_digits
-
-# def integer_concatination(input_digits):
-    
+                number = ''.join([char for char in line if char.isdigit()])                 # To create a list of numbers by omitting letters.
+                number_digits = list(number)
+                digits = [int(char) for char in number_digits]
+                input_numbers.append(digits)
+    return input_numbers
 
 def main():
     filename = 'input.txt'
-    digits = extract_digits(filename)
-    print(digits)
+    list_of_numbers = extract_input_numbers(filename)
+    print(list_of_numbers)
 
 if __name__ == "__main__":
     main()
